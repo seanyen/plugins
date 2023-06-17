064e5b91883b09665d610ab5deb0298c730ba3bd
Sean Yen <seanyen@microsoft.com>
2023-06-16 20:21:46 -0700

    go fmt.

--- main_windows.go
+++ main_windows.go
@@ -15,7 +15,7 @@ import (
 	bv "github.com/containernetworking/plugins/pkg/utils/buildversion"
 	hostlocal "github.com/containernetworking/plugins/plugins/ipam/host-local"
 	"github.com/containernetworking/plugins/plugins/main/windows/win-bridge"
-    "github.com/containernetworking/plugins/plugins/main/windows/win-overlay"
+	"github.com/containernetworking/plugins/plugins/main/windows/win-overlay"
 	"github.com/containernetworking/plugins/plugins/meta/flannel"
 )
 
